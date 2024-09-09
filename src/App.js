a50e392fcb1fca576f8be61673af9aa873807688
kapil <kc918062@gmail.com>
2024-09-09 10:22:48 +0530

    good one

---
+++ src/App.js
@@ -0,0 +1,26 @@
+// import logo from './logo.svg';
+import './App.scss';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout'
+import Home from './components/Home'
+import About from './components/About';
+import Portfolio from './components/Portfolio'
+import Contact from './components/Contact';
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />} >
+          <Route>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="portfolio" element={<Portfolio />} />
+          </Route>
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
